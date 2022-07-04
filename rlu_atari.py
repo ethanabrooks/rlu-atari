dcfd5497800d06baa0a0e3366cf478c5dadc9afe
Ethan Brooks <ethanabrooks@gmail.com>
2022-07-04 11:01:13 -0400

    about to minimize for tfds issue

--- rlu_atari.py
+++ rlu_atari.py
@@ -3,15 +3,20 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 import apache_beam as beam
 
-SPLIT_WEIGHTS = (6, 2, 2)
-# splits = tfds.Split.TRAIN.subsplit(weighted=SPLIT_WEIGHTS)
-downConfig = tfds.download.DownloadConfig(
-    beam_options=beam.options.pipeline_options.PipelineOptions(),
-    register_checksums=False,
+# If you are running on Dataflow, Spark,..., you may have to set-up runtime
+# flags. Otherwise, you can leave flags empty [].
+# flags = ['--runner=DataflowRunner', '--project=rlu-atari',]
+
+# `beam_options` (and `beam_runner`) will be forwarded to `beam.Pipeline`
+dl_config = tfds.download.DownloadConfig(
+    beam_options=beam.options.pipeline_options.PipelineOptions(
+        runner="DataflowRunner",
+        project="rlu-atari",
+        job_name="download-rlu-atari",
+        temp_location="gs://rlu-atari/temp",
+        region="us-east5",
+    )
 )
-ds = tfds.load(
-    "rlu_atari",
-    data_dir="/shared/home/ethanbro/tensorflow_datasets/",
-    # split=list(splits),
-    download_and_prepare_kwargs={"download_config": downConfig},
-)
+data_dir = "gs://rlu-atari/tensorflow_datasets"
+builder = tfds.builder("rlu_atari", data_dir=data_dir)
+builder.download_and_prepare(download_config=dl_config)
