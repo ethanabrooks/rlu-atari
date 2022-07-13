c9ab4df3ef6e156c7b8b182a7ddbf0f8c97dc26c
Ethan Brooks <ethanabrooks@gmail.com>
2022-07-13 15:18:22 -0400

    refactor rlu_atari and add batching

--- rlu_atari.py
+++ rlu_atari.py
@@ -2,27 +2,37 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import apache_beam as beam
-import os
 
 # If you are running on Dataflow, Spark,..., you may have to set-up runtime
 # flags. Otherwise, you can leave flags empty [].
 # flags = ['--runner=DataflowRunner', '--project=rlu-atari',]
 
 # `beam_options` (and `beam_runner`) will be forwarded to `beam.Pipeline`
+
+name = "rlu_atari"
+gcp_project = "rlu-atari"
+gcs_bucket = "gs://rlu-atari"
+
 dl_config = tfds.download.DownloadConfig(
     beam_options=beam.options.pipeline_options.PipelineOptions(
-        job_name=f"{os.getenv('GCP_PROJECT')}-gen",
-        project=os.getenv("GCP_PROJECT"),
+        job_name=f"{gcp_project}-gen",
+        project=gcp_project,
         region="us-east5",
         requirements_file="beam_requirements.txt",
         runner="DataflowRunner",
-        staging_location=f"{os.getenv('GCS_BUCKET')}/binaries",
-        temp_location=f"{os.getenv('GCS_BUCKET')}/temp",
+        staging_location=f"{gcs_bucket}/binaries",
+        temp_location=f"{gcs_bucket}/tmp",
     )
 )
-data_dir = f"{os.getenv('GCS_BUCKET')}/tensorflow_datasets"
-builder = tfds.builder(os.getenv("DATASET_NAME"), data_dir=data_dir)
+data_dir = f"{gcs_bucket}/tensorflow_datasets"
+builder = tfds.builder(name, data_dir=data_dir)
 builder.download_and_prepare(download_config=dl_config)
 ds = builder.as_dataset()
-for data in ds["train"]:
+for data in ds["train"].batch(8):
+    print(
+        "checkpoint_id",
+        data["checkpoint_id"].numpy(),
+        "episode_id",
+        data["episode_id"].numpy(),
+    )
     breakpoint()
